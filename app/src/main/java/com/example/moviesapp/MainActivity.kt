74b263c1be1c91363ef27e116598a04b74920bb5
Белецкий Вячеслав <bva@sibdigital.net>
2022-11-17 21:12:30 +0800

    commit

---
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.moviesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.example.moviesapp.ui.theme.MoviesAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MoviesAppTheme {
+                val navController = rememberNavController()
+
+            }
+        }
+    }
+}
+
